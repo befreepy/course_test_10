b011994c179df39fd7458ca7c40353964cdb0056
befreepy <spirouvika@gmail.com>
2023-01-06 09:35:59 +0200

    Initial commit

---
+++ utils.py
@@ -0,0 +1,41 @@
+import json
+
+"""
+Получаем список студентов
+"""
+
+def load_candidates():
+    with open('candidates.json', "r", encoding='utf8') as file:
+        return json.load(file)
+
+
+"""
+Показываем всех кандидатов
+"""
+
+def get_all():
+    return load_candidates()
+
+"""
+Возвращаем кандидата по pk
+"""
+
+def get_by_pk(pk):
+    for candidate in load_candidates():
+        if pk == candidate['id']:
+            return candidate
+    return
+
+"""
+Возвращаем кандидатов по навыку
+"""
+
+
+def get_by_skill(skill):
+    candidates = []
+    for candidate in load_candidates():
+        if skill.lower() in candidate['skills'].lower().split(', '):
+            candidates.append(candidate)
+    return candidates
+
+
